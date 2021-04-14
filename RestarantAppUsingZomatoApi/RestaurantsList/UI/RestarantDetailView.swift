a55da799a07750c8264af37a5334676ea63acb54
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-14 19:00:09 +0530

    complete search functionality, showing detail view, showing location on map using marker

---
+++ RestarantAppUsingZomatoApi/RestaurantsList/UI/RestarantDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RestarantDetailView.swift
+//  RestarantAppUsingZomatoApi
+//
+//  Created by Knoxpo MacBook Pro on 14/04/21.
+//
+
+import SwiftUI
+
+struct RestarantDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestarantDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestarantDetailView()
+    }
+}
